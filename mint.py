24762cba085e8510829c324fa232cf99796109e5
Horacio Gonzalez <hgonzalezl@gmail.com>
2021-01-05 11:01:03 -0600

    Update mint.py
    Avoid zombie processes

--- mint.py
+++ mint.py
@@ -135,7 +135,8 @@ mint = mintapi.Mint(
     mfa_method='soft-token',
     mfa_token=os.environ['MFA_TOKEN'],
     headless=True,
-    use_chromedriver_on_path=True
+    use_chromedriver_on_path=True,
+    wait_for_sync=False
 )
 
 invests = json.loads(mint.get_invests_json())
