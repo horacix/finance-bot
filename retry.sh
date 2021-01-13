59fe613fa6a47ea295031df415bc7389fe8080b6
Horacio Gonzalez <hgonzalezl@gmail.com>
2021-01-13 08:47:43 -0600

    Extend retry sleep

--- retry.sh
+++ retry.sh
@@ -6,8 +6,8 @@ MAX_ATTEMPTS=5
 attempt_num=1
 until python $1 || (( attempt_num == MAX_ATTEMPTS ))
 do
-    echo "Attempt $attempt_num failed! Trying again in $attempt_num seconds..."
-    sleep $(( attempt_num++ ))
+    echo "Attempt $attempt_num failed! Trying again in $attempt_num minutes..."
+    sleep $(( 60 * attempt_num++ ))
 done
 
 deactivate
