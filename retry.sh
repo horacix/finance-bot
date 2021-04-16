6e0d5a73c3a7a0f21405c5c95a9b129543f1c4fa
Horacio Gonzalez <hgonzalezl@gmail.com>
2021-04-16 13:57:24 -0500

    cleanup dangling processes

--- retry.sh
+++ retry.sh
@@ -11,6 +11,11 @@ until python $1 || (( attempt_num == MAX_ATTEMPTS ))
 do
     echo "Attempt $attempt_num failed! Trying again in $attempt_num minutes..."
     sleep $(( 60 * attempt_num++ ))
+    killall chromedriver
+    killall chromium-browse
 done
+
+killall chromedriver
+killall chromium-browse
 
 deactivate
